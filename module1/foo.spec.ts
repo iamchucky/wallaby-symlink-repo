89d1494d0e9349daa49f011cce19a66aca9dd81c
Chuck Yang <yangchuck@gmail.com>
2017-12-01 11:15:18 -0800

    Wallaby has issue when adding new method into file with es2015 module

--- module1/foo.spec.ts
+++ module1/foo.spec.ts
@@ -1,5 +1,11 @@
+// import { foo } from './foo';
+
 describe('test1', () => {
     it('return true', () => {
         expect(true).toBe(true);
+    });
+
+    it('return false', () => {
+        // expect(foo()).toBe(false);
     });
 });
